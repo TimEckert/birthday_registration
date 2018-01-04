edccad5a9754fd190b84edb040ffeefe7793ea36
TimEckert <TimEckert@users.noreply.github.com>
2018-01-04 18:12:38 +0100

    Tooltip toggle added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+var timesClicked = 0;
 
-$(document).ready(function() {
-  document.getElementById('tooltip').style.display = "none";
-});
+function DontHavePassword ()
+{
+  if (timesClicked == 0)
+  {
+    document.getElementById("tooltip").style.display = "block";
+    document.getElementById("btn_tooltip").innerHTML = "I still don't have a clue";
+    timesClicked++;
+  }
+  else if (timesClicked == 1)
+  {
+    document.getElementById("tooltip").innerHTML = "<p>Gosh, just try anything really.</p>";
+    timesClicked++;
+  }
+  else if (timesClicked == 2)
+      document.getElementById("tooltip").innerHTML = "<p>Gosh, just try <strong>anything</strong> really.</p>";
+
+
+}
