d0536e3f2c0abf65b8f33a24a89fbb2a864213b7
TimEckert <TimEckert@users.noreply.github.com>
2018-01-09 22:06:16 +0100

    Added form and summary functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // ----- Variables -----
 var timesClicked = 0;
-
+var formProgress = 0;   // 0 = no input yet, 4 = conpleted, needs to submit
+var summaryElements = [];
 // ----- Password Page -----
 function DontHavePassword ()
 {
@@ -16,7 +17,7 @@ function DontHavePassword ()
     timesClicked++;
   }
   else if (timesClicked == 2)
-    $("#tooltip").html ("<p>Gosh, just try <strong>anything</strong> really.</p>");
+    $("#tooltip").html ("<p>Gosh! Just try <strong>anything</strong> really.</p>");
 }
 
 function PasswordCheck ()
@@ -28,8 +29,42 @@ function PasswordCheck ()
 }
 
 // ----- Registration Page -----
-// $(".checkbox").change(function() {
-//     if(this.checked) {
-//         //Do stuff
-//     }
-// });
+function CheckboxEvent(checkboxNo)
+{
+  UpdateSummary(2, "in company, I will bring someone");
+
+  // Revealer
+  var level = 2;
+  if (checkboxNo >=2 && checkboxNo <= 5)
+    level = 3;
+  else if (checkboxNo > 5)
+    level = 4;
+  RevealSummary(level);
+}
+
+function DisplayName ()
+{
+    if (formProgress == 0)
+    {
+      formProgress = 1;
+      RevealSummary (formProgress);
+    }
+
+    var x = $("input[name=name]").val();
+    $("#response1 span").html(x);
+}
+
+function RevealSummary (level)
+{
+  summaryElements.push ( $("#response1"), $("#response2"), $("#response3"), $("#response4"));
+
+  level = (level > summaryElements.length) ? summaryElements.length : level;
+  for (var i = 0; i < level; i++)
+    summaryElements[i].css('display', 'block');
+}
+
+function UpdateSummary (responsePart, text)
+{
+  var element = "#response" + responsePart + " span";
+  $(element).html(text);
+}
