36f95d9cf276cdb4f569c5e519376fedb0b13e6c
TimEckert <TimEckert@users.noreply.github.com>
2018-01-10 22:34:34 +0100

    functionalitys complete styling first page and start of second

--- js/main.js
+++ js/main.js
@@ -31,7 +31,27 @@ function PasswordCheck ()
 // ----- Registration Page -----
 function CheckboxEvent(checkboxNo)
 {
-  UpdateSummary(2, "in company, I will bring someone");
+  // Switch, sorry for not refacturing
+  if ($('[name="plusOne"]').is(":checked"))
+    UpdateSummary(2, "in company, I will bring someone");
+  else
+    UpdateSummary(2, "alone, I will bring noone");
+
+  if ($(':radio[value=fifty]').is(":checked"))
+    UpdateSummary (3, "creative");
+  else if ($(':radio[value=hundred]').is(":checked"))
+    UpdateSummary (3, "normal");
+  else if ($(':radio[value=twohundred]').is(":checked"))
+    UpdateSummary (3, "generous");
+  else if ($(':radio[value=fifehundred]').is(":checked"))
+    UpdateSummary (3, "spectacular");
+
+  if ($(':radio[value=afternoon]').is(":checked"))
+    UpdateSummary (4, "for a coffee");
+  else if ($(':radio[value=night]').is(":checked"))
+    UpdateSummary (4, "and take a dance");
+  else if ($(':radio[value=both]').is(":checked"))
+    UpdateSummary (4, "the whole day");
 
   // Revealer
   var level = 2;
@@ -52,6 +72,7 @@ function DisplayName ()
 
     var x = $("input[name=name]").val();
     $("#response1 span").html(x);
+    $("#response4 #signature").html(x);
 }
 
 function RevealSummary (level)
@@ -65,6 +86,6 @@ function RevealSummary (level)
 
 function UpdateSummary (responsePart, text)
 {
-  var element = "#response" + responsePart + " span";
+  var element = "#response" + responsePart + " span:first";
   $(element).html(text);
 }
